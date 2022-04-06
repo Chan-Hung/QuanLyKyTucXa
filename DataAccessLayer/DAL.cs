8dd75fc59b3c66050dd393bb72ed56d42c4707f2
Chan-Hung <hungkevin19122001@gmail.com>
2022-04-06 07:59:25 +0700

    Add project files.

---
+++ DataAccessLayer/DAL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//ADO.NET
+using System.Data.SqlClient;
+using System.Data;
+
+namespace QuanLyKyTucXa.DataAccessLayer
+{
+    public class DAL
+    {
+        SqlConnection cnn;
+        SqlCommand cmd;
+        SqlDataAdapter adp;
+
+        string strConnect = "Data Source = (local); Initial Catalog = QuanLyKTX; Intergrated Security = True";
+
+        public DAL()
+        {
+            cnn = new SqlConnection(strConnect);
+            cmd = cnn.CreateCommand();
+        }
+
+        public DataSet ExecuteQueryDataSet(string strSQL, CommandType ct, params SqlParameter[] p)
+        {
+            cmd.CommandText = strSQL;
+            cmd.CommandType = ct;
+            adp = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adp.Fill(ds);
+            return ds;
+        }
+
+        public boo
+
+
+    }
+
+
+}
